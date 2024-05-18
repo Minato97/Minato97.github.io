9c1d99b2e32c9b4c3e608886090581591cc1ae48
Minato97 <ignacio.andrade8033@alumnos.udg.mx>
2024-05-18 14:13:48 -0600

    first commit

---
+++ app/Events/SARegistro.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class SARegistro implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+
+    public $registro;
+    public function __construct($registro)
+    {
+        $this->registro = $registro;
+//        dd($this->registro->estudiantes->id_tutores);
+
+    }
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new Channel('SmartAccess'),
+//            new Channel('SmartAccess.'.$this->registro['estudiantes']['id_tutores']),
+        ];
+    }
+}
---
+++ app/Http/Controllers/RegistroSent.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Events\SARegistro;
+
+use Illuminate\Http\Request;
+
+class RegistroSent extends Controller
+{
+    public function registro(Request $request)
+    {
+
+//        return $request->data1;
+//        dd($request->all());
+//        event(new testWebSocket);
+        broadcast(new SARegistro($request->registro));
+//
+        return ['mensaje' => 'si se envio'];
+//        return $request->registro['estudiantes']['id_tutores'];
+   }
+}
